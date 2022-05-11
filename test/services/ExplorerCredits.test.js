61304cf3b542ed3890670e4cf7510eadb6f1203b
AlexHz01 <yagamilonzoro@gmail.com>
2022-05-10 21:51:10 -0500

    Agregando Una peuqeña modi a el package

--- test/services/ExplorerCredits.test.js
+++
@@ -1,9 +0,0 @@
-const ExplorerCredits = require("../../lib/services/ExplorerCredits")
-
-describe("Prueba 3", () => {
-    test("prueba de accreditation mayor a 500", ()=>{
-        const explorer = {id : "kike" , credits : 600 };
-        const Credits = ExplorerCredits.explorerCreditsNeeded(explorer)
-        expect(Credits).toEqual({id : "kike" , credits : 600 })
-    })
-})
