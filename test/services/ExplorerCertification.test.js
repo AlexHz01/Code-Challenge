7c60fb5ff25d60a4964b4a216bb3a509cc62f354
AlexHz01 <yagamilonzoro@gmail.com>
2022-05-10 21:53:47 -0500

    Agregando studens Services + su test de prueba

--- test/services/ExplorerCertification.test.js
+++ test/services/ExplorerCertification.test.js
@@ -1,9 +1,9 @@
-const  ExplorerCertification = require("../../lib/services/ExplorerCertification")
+const  ExplorerCertification = require("../../lib/services/StudentsCertification")
 
 describe("Prueba 2 Certification de explorer", () =>{
     test("test de certification", () =>{
-        const explorer = [{ haveCertification : true }, {haveCertification: false}]
+        const explorer = [{ haveCertification : true, email : "kike@gmail.com" }, {haveCertification: false, email : "Riusey@gmail.com"},{haveCertification: false, email : "Riusey@gmail.com"}]
         const certification = ExplorerCertification.haveCertifications(explorer, true)
-        expect(certification).toEqual ([{ haveCertification : true }])
+        expect(certification).toEqual (["kike@gmail.com"])
     })
 })
